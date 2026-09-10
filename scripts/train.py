423c89a200cae175706b3b546ea2844056119407
Bruce Kang <brucekangcn@gmail.com>
2026-09-11 00:18:00 +0800

    feat: add data command line option

--- scripts/train.py
+++ scripts/train.py
@@ -7,6 +7,7 @@ PROJECT_ROOT = Path(__file__).resolve().parent.parent
 
 parser = argparse.ArgumentParser()
 parser.add_argument("model", nargs="?", default="yolo26n.pt")
+parser.add_argument("-d", "--data", default="data.yaml")
 parser.add_argument("-r", "--resume", action="store_true")
 
 args = parser.parse_args()
@@ -15,7 +16,7 @@ model = YOLO(args.model)
 task = model.task if model.task is not None else "detect"
 
 results = model.train(
-    data="data.yaml",
+    data=args.data,
     cfg=PROJECT_ROOT / "hyp.yaml",
     project=PROJECT_ROOT / "runs" / task,
     resume=args.resume,
